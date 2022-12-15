e630d2db699e5f90b928ddede1c518991d3ca69c
Igor <you@example.com>
2022-12-15 09:08:12 +0300

    Add new code

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -7,13 +7,33 @@
 
 Console.Clear();
 
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
 int size = 4;
-int[,] array = new int[size,size]
+int[,] array = new int[size, size];
+int count = 1;
 
 for (int i = 0; i < size; i++)
 {
-    for (int j = 0; j < size; j++)
-    {
-        
+    // for (int j = 0; j < size; j++)
+    // {
+    //     if (((i == 0) && (j < size - 1)) || (j == size - 1))
+    //         array[i, j] = count;
+
+
+
+        count++;
     }
 }
+
+PrintArray(array);
